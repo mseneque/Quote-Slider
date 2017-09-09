cb27acbc03be1f28169344e607defc150b9bc66f
mseneque <mattseneque@gmail.com>
2017-09-09 12:06:46 +0800

    initial commit

---
+++ 2-Quote-Slider/_2_Quote_Slider/QuotesPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _2_Quote_Slider
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        public String[] Quotes;
+        private int _index;
+
+        public QuotesPage()
+        {
+            _index = 0;
+            Quotes = new[]
+            {
+                "Quote 1",
+                "Quote 2",
+                "Quote 3",
+                "Quote 4",
+                "Quote 5",
+                "Quote 6"
+            };
+
+            InitializeComponent();
+
+            Label.Text = Quotes[0];
+
+
+        }
+
+        private void Next_OnClicked(object sender, EventArgs e)
+        {
+            _index++;
+            if (_index >= Quotes.Length)
+                _index = 0;
+            Label.Text = String.Format("{0}",  Quotes[_index]);
+        }
+    }
+}
